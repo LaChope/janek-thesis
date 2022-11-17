69cbdd417928a3c742874ab8a040d7aef06560d4
Max Chopart <max.chopart@gmail.com>
2022-11-17 15:23:08 +0100

    Baro data done

--- LKPR_pressure.py
+++ LKPR_pressure.py
@@ -1,23 +1,24 @@
 import pandas as pd
 
-
 def clean_dataset(dataframe):
     dataframe.columns = dataframe.iloc[1]
     dataframe = dataframe.iloc[2:, :]
     dataframe["PA_QNH (HPA)"] = dataframe["PA_QNH (HPA)"].str.replace(',', '.').astype("float")
-    return dataframe
+    dataframe["CREATEDATE"] = pd.to_datetime(dataframe["CREATEDATE"], infer_datetime_format=True)
+    return dataframe[["CREATEDATE", "PA_QNH (HPA)"]]
 
 
 def get_mean_value(dataframe):
-    return dataframe["PA_QNH (HPA)"].groupby(dataframe["PA_QNH (HPA)"].index // 30).mean()
+    dataframe["MEAN_30"] = dataframe["PA_QNH (HPA)"].groupby(dataframe["PA_QNH (HPA)"].index // 30).mean()
+    return dataframe
 
 
 def get_pressure_dataframe(file):
     df = pd.DataFrame(pd.read_excel(file))
     df = clean_dataset(df)
-    df = get_mean_value(df)
+    # df = get_mean_value(df)
     df = pd.DataFrame(df)
-    df.insert(0, "Time (h)", (df.index * 30) / 60)
+    # df.insert(0, "Time (h)", (df.index * 30) / 60)
     return df
 
 
