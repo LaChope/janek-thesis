d866d42833c49229945d94a20e65d4faa5a301b9
Max Chopart <max.chopart@gmail.com>
2022-11-17 18:03:49 +0100

    Fix cleaning data

--- LKPR_pressure.py
+++ LKPR_pressure.py
@@ -1,15 +1,16 @@
 import pandas as pd
+import numpy as np
 import glob
 import matplotlib.pyplot as plt
 
-BARO_DATA = glob.glob("./data/*/*/BARO/PRESSURE_1_*.his")
+BARO_DATA = glob.glob("./data/*/*/BARO/*.his")
 KEY = "PA_QNH (HPA)"
 CREATE_DATE = "CREATEDATE"
 
-print(len(BARO_DATA))
 
 def clean_dataset(dataframe):
     dataframe[CREATE_DATE] = pd.to_datetime(dataframe[CREATE_DATE], infer_datetime_format=True)
+    dataframe = dataframe[dataframe[KEY].apply(lambda x: isinstance(x, (float, np.float32)))]
     return dataframe[[CREATE_DATE, KEY]]
 
 
@@ -29,12 +30,11 @@ def get_pressure_dataframe(file):
 
 if __name__ == '__main__':
     df_list = []
-    # for i in range(len(BARO_DATA)):
-    for i in range(170):
+    for i in range(len(BARO_DATA)):
         df = get_pressure_dataframe(str(BARO_DATA[i]))
         df_list.append(df)
     concat_df = pd.concat(df_list)
     plt.scatter(concat_df[CREATE_DATE], concat_df[KEY])
-    plt.ylabel("Pressure (HPA)")
+    plt.ylabel(KEY)
     plt.show()
     concat_df.to_excel("./PRESSURE.xlsx")
--- LKPR_temperature.py
+++ LKPR_temperature.py
@@ -1,26 +1,28 @@
 import pandas as pd
+import numpy as np
 import glob
 import matplotlib.pyplot as plt
 
-TEMP_DATA = glob.glob("./data/2021/*/TEMP/TEMP_MET_01*.his")
-KEY = "THT_TT"
+TEMP_DATA = glob.glob("./data/2021/*/TEMP/*.his")
+KEY = "TH_TT"
 CREATE_DATE = "CREATEDATE"
 
 
 
 def clean_dataset(dataframe):
     dataframe[CREATE_DATE] = pd.to_datetime(dataframe[CREATE_DATE], infer_datetime_format=True)
+    dataframe = dataframe[dataframe[KEY].apply(lambda x: isinstance(x, (float, np.float32)))]
     return dataframe[[CREATE_DATE, KEY]]
 
 
-def get_mean_value(dataframe, key):
-    dataframe["MEAN_30"] = dataframe[key].groupby(dataframe[key].index // 30).mean()
+def get_mean_value(dataframe):
+    dataframe["MEAN_30"] = dataframe[KEY].groupby(dataframe[KEY].index // 30).mean()
     return dataframe
 
 
 def get_pressure_dataframe(file):
     data = pd.DataFrame(pd.read_csv(file, delimiter='\t', engine="python", skiprows=1))
-    data = clean_dataset(data, KEY)
+    data = clean_dataset(data)
     # df = get_mean_value(df)
     data = pd.DataFrame(data)
     # df.insert(0, "Time (h)", (df.index * 30) / 60)
@@ -30,11 +32,10 @@ def get_pressure_dataframe(file):
 if __name__ == '__main__':
     df_list = []
     for i in range(len(TEMP_DATA)):
-    # for i in range(2):
         df = get_pressure_dataframe(str(TEMP_DATA[i]))
         df_list.append(df)
     concat_df = pd.concat(df_list)
     plt.scatter(concat_df[CREATE_DATE], concat_df[KEY])
     plt.ylabel("Temp")
     plt.show()
-    # concat_df.to_excel("./TEMPERATURE.xlsx")
+    concat_df.to_excel("./TEMPERATURE.xlsx")
